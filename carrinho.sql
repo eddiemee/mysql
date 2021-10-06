1ab87dd7010a2e5bd8881f96cbcb720af6093a98
eddiemee <73147293+eddiemee@users.noreply.github.com>
2021-10-06 20:34:27 -0300

    subi os arquivos do MySql

--- carrinho.sql
+++ carrinho.sql
@@ -61,19 +61,67 @@ create table estoque(
 describe estoque;
 
 -- inserir apenas os itens não automáticos
-insert into estoque(produto,fabricante,dataval,quantidade,estoquemin,
+insert into estoque(produto,barcode,fabricante,dataval,quantidade,estoquemin,
 medida,valor,loc)
 values
-('Caneta BIC azul','BIC',20221005,100,10,'CX',28.75,'setor A p2');
+('Caneta BIC azul','BIC',20221005,9,10,'CX',28.75,'setor A p2');
 
-insert into estoque(produto,fabricante,dataval,quantidade,estoquemin,
+insert into estoque(produto,barcode,fabricante,dataval,quantidade,estoquemin,
 medida,valor,loc)
 values
 ('Borracha Faber verde','Faber',20251005,100,5,'CX',9.99,'setor B p3');
 
+insert into estoque(produto,barcode,fabricante,dataval,quantidade,estoquemin,
+medida,valor,loc)
+values
+('Réguas Jogo 10','Faber',20191005,30,3,'CX',29.99,'setor B p3');
+
+insert into estoque(produto,barcode,fabricante,dataval,quantidade,estoquemin,
+medida,valor,loc)
+values
+('Fita durex','durex',20201005,50,10,'CX',19.99,'setor B p2');
+
+insert into estoque(produto,barcode,fabricante,dataval,quantidade,estoquemin,
+medida,valor,loc)
+values
+('Cola Bastão','Polar',20261005,4,5,'CX',9.99,'setor B p3');
+
 delete from estoque where codigo=2;
 
 select * from estoque;
+
+-- Inventário do estoque (total)
+select sum(quantidade * valor) as total from estoque;
+
+-- Relatório de reposição do estoque 1
+select * from estoque where quantidade < estoquemin;
+
+-- Relatório de reposição do estoque 2
+-- date_format() -> formatar a exibição da data
+-- %d (dia) %m (mês) %y (ano 2 dígitos) %Y (ano 4 dígitos)
+
+select codigo as código,produto,
+date_format (dataval,'%d/%m/%Y') as data_validade,quantidade,estoquemin as estoque_mínimo
+from estoque where quantidade < estoquemin;
+
+-- Relatório de produtos vencidos 1
+select codigo as código,produto,
+date_format(dataval,'%d/%m/%Y') as data_validade
+from estoque;
+
+-- Relatório de produtos vencidos 2
+-- datediff() retorna a diferença em dias de duas datas
+-- curdate() data atual
+select codigo as código,produto,
+date_format(dataval,'%d/"m/%Y') as data_validade,
+datediff(dataval,curdate()) as dias_restantes
+from estoque;
+
+update estoque set quantidade = '99' where codigo=1;
+update estoque set produto = 'Borracha Faber Azul', fabricante = 'Faber Castell', datacad='20191006', dataval='20221006', quantidade='95', estoquemin='3', medida='UN', valor='8.99', loc='setor A p2' where codigo='3';
+
+
+-- 
 
 select * from carrinho order by produto;
 select codigo, produto from carrinho order by codigo desc;
@@ -81,6 +129,7 @@ select codigo, produto from carrinho order by codigo desc;
 -- CRUD Update
 update carrinho set quantidade = '20' where codigo=1;
 update carrinho set valor = '50.00' where codigo=3;
+
 
 -- CRUD delete
 delete from carrinho where codigo=5;
